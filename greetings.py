0e7fd59b71056590b9a778716286ad92c9ab632d
Anne Muchiri <160058414+Annemuchiri@users.noreply.github.com>
2024-02-28 17:34:57 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,5 @@
+# user_input.py
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+print(f"Hello {"anne"}, you are {23} years old and live in {"nairobi"}.")
